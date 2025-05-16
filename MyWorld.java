a9c814b2d3459dfd9a93ab97241a9f96d8b2547d
ComplexUsernames <349462549@gapps.yrdsb.ca>
2025-05-16 09:20:27 -0400

    Add balloons to world

--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,12 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        RedBalloon balloon1 = new RedBalloon();
+        addObject(balloon1, 300, 300);
+        BlueBalloon balloon2 = new BlueBalloon();
+        addObject(balloon2, 350, 300);
+        YellowBalloon balloon3 = new YellowBalloon();
+        addObject(balloon3, 250, 300);
     }
 }
