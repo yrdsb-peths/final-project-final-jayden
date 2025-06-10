cc5888bc892bb3b2986bc22cf1e7ecf834e04ed9
ComplexUsernames <349462549@gapps.yrdsb.ca>
2025-06-10 09:25:35 -0400

    Update Tutorial.java

--- Tutorial.java
+++ Tutorial.java
@@ -10,8 +10,9 @@ public class Tutorial extends World
 {
     //Label implementations
     Label tutorial1 = new Label("Slice the fruits with your mouse", 40);
-    Label tutorial2 = new Label("before they fall to the floor", 40);
-    Label tutorial3 = new Label("Don't slice bombs", 40);
+    Label tutorial2 = new Label("by holding left click", 40);
+    Label tutorial3 = new Label("before they fall to the floor", 40);
+    Label tutorial4 = new Label("Don't slice bombs", 40);
     
     Rectangle returnRect = new Rectangle(100, 50);
     Label returnLabel = new Label("Return", 40);
@@ -30,6 +31,7 @@ public class Tutorial extends World
         addObject(tutorial1, getWidth()/2, getHeight()/2-150);
         addObject(tutorial2, getWidth()/2, getHeight()/2-75);
         addObject(tutorial3, getWidth()/2, getHeight()/2);
+        addObject(tutorial4, getWidth()/2, getHeight()/2+75);
         
         addObject(returnRect, 75, 350);
         addObject(returnLabel, 75, 350);
